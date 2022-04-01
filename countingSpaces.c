3adb9d957377d2c6fd3ec160ecf0fac1dd9ce8de
maxxboykin <maxericboykin@gmail.com>
2022-04-01 19:21:19 -0400

    move files around

---
+++ countingSpaces.c
@@ -0,0 +1,36 @@
+//
+// Created by Max Boykin on 4/1/22.
+//
+
+//
+// Created by Max Boykin on 3/31/22.
+//
+
+/*
+ * copy input to output
+ */
+
+#include <stdio.h>
+
+#define IN 1 /* inside a word */
+#define OUT 0 /* outside a word */
+/* count lines, words, and characters in input */
+
+main() {
+    int c, nl, nw, nc, state;
+
+    state = OUT;
+    nl = nw = nc = 0;
+    while ((c = getchar()) != EOF) {
+        ++nc;
+        if (c == '\n')
+            ++nl;
+        if (c == ' ' || c == '\n' || c == '\t')
+            state = OUT;
+        else if (state == OUT) {
+            state = IN;
+            ++nw;
+        }
+    }
+    printf("%d %d %d\n", nl, nw, nc);
+}

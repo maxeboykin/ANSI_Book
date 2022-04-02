283d0294b09386b23d38f6acabdc215cf5f46655
maxxboykin <maxericboykin@gmail.com>
2022-04-02 16:48:54 -0400

    maxline example

--- main.c
+++ main.c
@@ -1,20 +1,30 @@
-//
-// Created by Max Boykin on 3/31/22.
-//
 
-/*
- * copy input to output
- */
 
 #include <stdio.h>
-int power(int m, int n);
-int fahrenheitCelsiusTable(int lower, int upper, int step);
-/* test power function*/
+#define MAXLINE 1000 /* maximum input line size */
 
+int getline(char line[], int maxline);
+void copy(char to[], char from[]);
+
+/* print longest input line */
 main()
 {
+    int len; /*current line length */
+    int max; /*maximum length so far */
+    char line[MAXLINE]; /* current input line */
+    char longest[MAXLINE]; /*longest line saved here */
 
+    max = 0;
+    while((len = getline(line, MAXLINE)) > 0)
+        if (len > max) {
+            max = len;
+            copy(longest, line);
+        }
+    if (max > 0) /*there was a line */
+        printf("%s", longest);
     return 0;
 }
+
+
 
 

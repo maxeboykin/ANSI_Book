dc22c5aa83537eb357b16a1b5db20797c978799e
maxxboykin <maxericboykin@gmail.com>
2022-04-05 20:32:56 -0400

    strcat function

--- atoi_lower.c
+++ atoi_lower.c
@@ -26,3 +26,56 @@ int lower(int c)
     else
         return c;
 }
+
+int htoi(char s[])
+{
+    int i = 0;
+    if (s[i] == '0')
+    {
+        ++i;
+    }
+    if (s[i] == 'x' || s[i] == 'X')
+    {
+        ++i;
+    }
+
+    int n = 0;
+    int hexdigit;
+
+    for(; i < strlen(s); ++i)
+    {
+        if (s[i] >= '0' && s[i] <= '9'){
+            hexdigit = s[i] - '0';
+            n = 16 * n + hexdigit;
+        }
+        if(s[i] >= 'A' && s[i] <= 'F'){
+            hexdigit = s[i] - 'A' + 10;
+            n = 16 * n + hexdigit;
+        }
+        if (s[i] >= 'a' && s[i] <= 'f'){
+            hexdigit = s[i] - 'a' + 10;
+            n = 16 * n + hexdigit;
+        }
+    }
+    printf("%d", n);
+    return n;
+}
+
+/*strcat: concatenate t to end of s; s must be big enough */
+void strcat(char s[], char t[])
+{
+    int i , j;
+    i = j = 0;
+    while (s[i] != '\0') /* find end of s */
+        i++;
+    while ((s[i++] = t[j++]) != '\0'); /* copy t */
+}
+
+
+int randomFunctions()
+{
+    char a[] = "0x71927";
+    htoi(a);
+
+    return 0;
+}

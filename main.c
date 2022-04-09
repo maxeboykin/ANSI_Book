3cdf2428916506c2fc86a9e0ac686ad07f53d322
maxxboykin <maxericboykin@gmail.com>
2022-04-09 18:31:13 -0400

    reverse Polish calc part 1

--- main.c
+++ main.c
@@ -1,9 +1,16 @@
 
 #include<stdio.h>
+#include <stdlib.h>
 #include<string.h>
-int strrindex(char s[], char t[]);
-int atoiCustom(char s[]);
 
+#define MAXOF 100 /* max size of operand or operator */
+#define NUMBER '0' /* signal that a number was found */
+
+int getop(char []);
+void push(double);
+double pop(void);
+
+/* reverse Polish calculator */
 main ()
 {
 
